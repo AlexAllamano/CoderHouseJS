184635c739de7df727be2c97dc19a8eaafeefa80
Alex Allamano <alexisallamano@hotmail.com>
2022-11-06 12:51:48 -0300

    Commit - Agrego el Fetch

--- script.js
+++ script.js
@@ -31,30 +31,26 @@ let tituloTarjetas = document.getElementById('tituloTarjetas');
 
 // Inicializo un arreglo de ejemplos cargado, y el arreglo filtrado que se carga con el localStorage
 let arregloMascotas = [
-    mascota1 = new Mascota('Lenny', 2, 'Gato', 'Macho', 'Chiquito y mimoso, blanquito', 'Río Grande 76', 'https://www.instagram.com/', 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/Purina%C2%AE%20La%20llegada%20del%20gatito%20a%20casa.jpg?itok=_3VnSPSl'),
-    mascota2 = new Mascota('León', 2, 'Perro', 'Macho', 'Peludo, juguetón e intenso', 'Gral Paz 120', 'https://www.instagram.com/', 'https://thumbs.dreamstime.com/b/perro-de-perrito-en-el-parque-50880187.jpg'),
-    mascota3 = new Mascota('Ramón', 5, 'Gato', 'Macho', 'Viejito, pero el más compañero', 'Punta del Sauce 2318', 'https://www.instagram.com/', 'https://www.clarin.com/img/2022/05/03/hEUHNtPrq_360x240__1.jpg'),
-    mascota4 = new Mascota('Flopy', 3, 'Perro', 'Hembra', 'La más fiel de todas', 'Buenos Aires 495', 'https://www.instagram.com/', 'https://okdiario.com/img/2020/06/17/5-aspectos-basicos-sobre-el-embarazo-de-tu-perrita-655x368.jpg'),
-    mascota5 = new Mascota('Lucy', 2, 'Perro', 'Hembra', 'Chiquitita, vive en depto!', 'Obispo Oro 43', 'https://www.instagram.com/', 'https://nombres-para.wiki/wp-content/uploads/2018/06/Nombres-para-perritas.jpg'),
-    mascota6 = new Mascota('Lima', 2, 'Gato', 'Hembra', 'Micha castrada peluda, le gusta bañarse', 'Pasaje Fournier 22', 'https://www.instagram.com/', 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/como-alimentar-una-gata-durante-el-embarazo.jpg?itok=HGnK-t-y'),
-    mascota7 = new Mascota('Ichi', 2, 'Gato', 'Macho', 'Es tímido pero muy mimoso', 'Salta 542', 'https://www.instagram.com/', 'https://blog.mascotaysalud.com/wp-content/uploads/2015/08/gato-recien-nacido.jpg'),
-    mascota8 = new Mascota('Chicho', 2, 'Perro', 'Macho', 'No hay perro más obediente', 'Cachiyaco 533', 'https://www.instagram.com/', 'https://us.123rf.com/450wm/olyamax/olyamax2006/olyamax200600018/149899027-perrito-divertido-asoma-la-nariz-a-la-c%C3%A1mara-lindo-perro-perdiguero-de-seis-semanas-de-edad-mirando-.jpg?ver=6'),
-    mascota9 = new Mascota('Tosty', 2, 'Gato', 'Hembra', 'Muy hiperactiva', 'Velez Sasrfield 5000', 'https://www.instagram.com/', 'https://www.tqpets.com.ec/wp-content/uploads/2019/08/gatita_longeva_fb.png'),
-    mascota10 = new Mascota('Yuki', 2, 'Gato', 'Hembra', 'La más fotogénica', 'Gobernación 105', 'https://www.instagram.com/', 'https://nfnatcane.es/blog/wp-content/uploads/2019/07/gatita-con-hermosos-ojos-verdes.jpg'),
-    mascota11 = new Mascota('Manolo', 2, 'Gato', 'Macho', 'Mimoso y charlatán', 'Bower 54', 'https://www.instagram.com/', 'http://mascotafiel.com/wp-content/uploads/2016/10/que-raza-son-los-gatos-naranja.jpg'),
+    mascota1 = new Mascota('Lenny', 2, 'Gato', 'Macho', 'Chiquito y mimoso, blanquito', 'Río Grande 76', 'https://www.instagram.com/', obtenerImagenUrl('gato')),
+    mascota2 = new Mascota('León', 2, 'Perro', 'Macho', 'Peludo, juguetón e intenso', 'Gral Paz 120', 'https://www.instagram.com/', obtenerImagenUrl('perro')),
+    mascota3 = new Mascota('Ramón', 5, 'Gato', 'Macho', 'Viejito, pero el más compañero', 'Punta del Sauce 2318', 'https://www.instagram.com/', obtenerImagenUrl('gato')),
+    mascota4 = new Mascota('Flopy', 3, 'Perro', 'Hembra', 'La más fiel de todas', 'Buenos Aires 495', 'https://www.instagram.com/', obtenerImagenUrl('perro')),
+    mascota5 = new Mascota('Lucy', 2, 'Perro', 'Hembra', 'Chiquitita, vive en depto!', 'Obispo Oro 43', 'https://www.instagram.com/', obtenerImagenUrl('perro')),
+    mascota6 = new Mascota('Lima', 2, 'Gato', 'Hembra', 'Micha castrada peluda, le gusta bañarse', 'Pasaje Fournier 22', 'https://www.instagram.com/', obtenerImagenUrl('gato')),
+    mascota7 = new Mascota('Ichi', 2, 'Gato', 'Macho', 'Es tímido pero muy mimoso', 'Salta 542', 'https://www.instagram.com/', obtenerImagenUrl('gato')),
+    mascota8 = new Mascota('Chicho', 2, 'Perro', 'Macho', 'No hay perro más obediente', 'Cachiyaco 533', 'https://www.instagram.com/', obtenerImagenUrl('perro')),
+    mascota9 = new Mascota('Tosty', 2, 'Gato', 'Hembra', 'Muy hiperactiva', 'Velez Sasrfield 5000', 'https://www.instagram.com/', obtenerImagenUrl('gato')),
+    mascota10 = new Mascota('Yuki', 2, 'Gato', 'Hembra', 'La más fotogénica', 'Gobernación 105', 'https://www.instagram.com/', obtenerImagenUrl('gato')),
+    mascota11 = new Mascota('Manolo', 2, 'Gato', 'Macho', 'Mimoso y charlatán', 'Bower 54', 'https://www.instagram.com/', obtenerImagenUrl('gato')),
 ];
 
 arregloMascotas = JSON.parse(localStorage.getItem('arregloMascotas')) || arregloMascotas;
-
-console.log(arregloMascotas);
-
 let arregloFiltrado = JSON.parse(localStorage.getItem('arregloFiltrado')) || [];
 
 
 
 let hizoBusqueda = false;
 dibujarTarjetas(arregloFiltrado);
-
 
 
 
@@ -126,6 +122,30 @@ function Mascota(nombre, edad, tipo, genero, anotaciones, direccion, contacto, i
 //Funciones 
 //Funciones 
 //Funciones 
+
+function obtenerImagenUrl(tipo){
+    let datos = obtenerImagen(tipo).then(data => console.log(data));
+    console.log(datos);
+    return datos;
+ }
+
+async function obtenerImagen(tipo) {
+    let imagen; 
+
+    if (tipo.toLowerCase() == 'gato') {
+        imagen = await fetch('https://aws.random.cat/meow').then(resp => resp.json()).then(data => data.file);
+        
+        return imagen;
+    } else {
+        imagen = await fetch('https://random.dog/woof.json').then(resp => resp.json()).then(data => data.url);
+        
+        return imagen;
+    }
+}
+
+
+
+
 
 
 function validar() {
@@ -265,11 +285,11 @@ function dibujarTarjetas(arreglo) {
             observer: true,
             loop: true
         });
-    
+
     });
 
 
-    
+
 
 }
 
